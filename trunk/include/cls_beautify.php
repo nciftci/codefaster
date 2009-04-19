c64f31ec3c2c48cb68b03e5ac9410aade423e56e
lvalics <lvalics@5a6d9080-2914-11de-b161-0f7b0ef88f65>
2009-04-19 16:24:10 +0000

    \n\n\n\ temporary solution

--- trunk/include/cls_beautify.php
+++ trunk/include/cls_beautify.php
@@ -2,7 +2,8 @@
 
 class Beautify{
 	function get_beautify_php($php_file_data){
-		$tmpfilename=@tempnam(sys_get_temp_dir(),"class_generator_");
+	return trim(str_replace("\n\n\n\n\n","\n",$php_file_data));
+		$tmpfilename=@tempnam("tmp/","class_generator_");
 		$handle = @fopen($tmpfilename, "w");
 		$result=$php_file_data;
 		if ($handle){	
@@ -12,10 +13,9 @@ class Beautify{
 			$extension="";
 			if (stristr(PHP_OS, 'WIN')) { 
 				$extension=".exe";
-			};
-
+			}
 			$outfilename="$tmpfilename.out";
-			$tmp=@system("./beautifier/phpCB$extension  --space-after-if --space-after-switch --space-after-while --space-before-start-angle-bracket --space-after-end-angle-bracket --one-true-brace-function-declaration --glue-amperscore --change-shell-comment-to-double-slashes-comment --force-large-php-code-tag --extra-padding-for-case-statement --force-true-false-null-contant-lowercase --align-equal-statements --comment-rendering-style PHPDoc --equal-align-position 50 --padding-char-count 4 $tmpfilename > '$outfilename'",$return);
+			$tmp=@system(DEFAULT_PATH."/beautifier/phpCB$extension  --space-after-if --space-after-switch --space-after-while --space-before-start-angle-bracket --space-after-end-angle-bracket --one-true-brace-function-declaration --glue-amperscore --change-shell-comment-to-double-slashes-comment --force-large-php-code-tag --extra-padding-for-case-statement --force-true-false-null-contant-lowercase --align-equal-statements --comment-rendering-style PHPDoc --equal-align-position 50 --padding-char-count 4 \"$tmpfilename\" > \"$outfilename\"",$return);
 			@unlink($tmpfilename);
 			if ($return==0) {
 				$result=$tmp;
@@ -23,14 +23,15 @@ class Beautify{
 				if ($outhandle){
 					$result=fread($outhandle,filesize($outfilename));
 					fclose($outhandle);
-				};
+				}
 				@unlink($outfilename);
-			};
-		};
+			}
+		}
 		return $result;
 	}
 	function beautify_html($html_file_data){
-		$tmpfilename=@tempnam(sys_get_temp_dir(),"class_generator_html_");
+	return trim(str_replace("\n\n\n\n\n","\n",$html_file_data));
+		$tmpfilename=@tempnam("tmp/","class_generator_html_");
 		$handle = @fopen($tmpfilename, "w");
 		$result=$html_file_data;
 		$orig_result=$result;
@@ -41,28 +42,28 @@ class Beautify{
 			$extension="";
 			if (stristr(PHP_OS, 'WIN')) { 
 				$extension=".exe";
-			};
+			}
 
 			$outfilename=$tmpfilename.".out";
-			$tmp=@system("./beautifier/tidy$extension  -config './beautifier/validator-config.txt' $tmpfilename > $outfilename",$return);
+			$tmp=@system(DEFAULT_PATH."beautifier/tidy$extension  -config \"./beautifier/validator-config.txt\" \"$tmpfilename\" > \"$outfilename\"",$return);
 			@unlink($tmpfilename);
+			
 			if ($return<127) {
 				$result=$tmp;
 				$outhandle = @fopen($outfilename, "r");
 				if ($outhandle){
-					$result=fread($outhandle,filesize($outfilename));
+					$result=@fread($outhandle,filesize($outfilename));
 					$delete_array=array( "<html>", "</html>", "<head>", "</head>", "<body>", "</body>", "<title>", "</title>"); 
 					foreach($delete_array as $d){
 						$result=str_ireplace($d,"",$result);
-					};
+					}
 					fclose($outhandle);
-				};
+				}
 				@unlink($outfilename);
-			};
-		};
+			}
+		}
 		if (empty($result)) return $orig_result;
 		return $result;
 	}
-};
-//TODO: $result_php=trim(str_replace("\n\n\n\n\n","\n",$ft->fetch("BODY")));
+}
 ?>
