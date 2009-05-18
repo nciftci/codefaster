79f24f74ca6a57f5291da7866cdcde3b784e5ba8
lvalics <lvalics@5a6d9080-2914-11de-b161-0f7b0ef88f65>
2009-05-18 17:12:23 +0000

    Add new row, Replace row, Disable mod & delete.

--- public_html/include/cls_listing.php
+++ public_html/include/cls_listing.php
@@ -1,4 +1,5 @@
 <?php
+
 	class Listing {
 		protected $sql;
 		protected $rs;
@@ -17,7 +18,11 @@
 		protected $activestyle;
 		protected $buttonstyle;
 		protected $limit_display;
-		
+		protected $enableMod;
+		protected $enableDel;
+		protected $replace_table;
+		protected $extra_fields;
+
 		public function __construct($query) {
 			$this->offset=0;
 			$this->page=1;
@@ -27,10 +32,37 @@
 			$this->rs=mysql_query($this->sql);
 			$this->numrows=mysql_num_rows($this->rs);
 			$this->numfields=mysql_num_fields($this->rs);
+			$this->enableDel = 0;
+			$this->enableMod = 0;
+			$this->replace_table=array();
+			$this->extra_fields=array();
+
 		}
+
+		public function getEnableMod()
+		{
+			return $this->enableMod;
+		}
+
+		public function setEnableMod($mod)
+		{
+			$this->enableMod = $mod;
+		}
+
+		public function getEnableDel()
+		{
+			return $this->enableDel;
+		}
+
+		public function setEnableDel($del)
+		{
+			$this->enableDel = $del;
+		}
+
 		public function getNumRows() {
 			return $this->numrows;
 		}
+
 		public function setLimit($no) {
 			$this->limit=$no;
 		}
@@ -40,9 +72,9 @@
         }
 		public function getFirstID() {
 			return $this->firstid;
-		}	
+		}
 		// ID field name
-		// end item activate exist	
+		// end item activate exist
         public function setActivateListing($idlist) {
             $this->activatelisting=$idlist;
         }
@@ -55,8 +87,8 @@
         }
 		public function getActivateFieldName() {
 			return $this->activename;
-		}		
-		// end item activate exist			
+		}
+		// end item activate exist
 		public function getLimit() {
 			return $this->limit;
 		}
@@ -92,62 +124,101 @@
 		public function listPages() {
 			$limited=$this->sql." limit ".$this->offset." , ".$this->limit;
 			$this->rs=mysql_query($limited) or die(mysql_error());
-			
+
 			if(strstr(mysql_field_flags($this->rs, 0),"primary_key")==false || mysql_field_type($this->rs,0)!="int")
 				return false;
 			$stringutil = new String();
-			
+
 			$data="<table width=\"99%\" cellspacing=\"0\" align=\"center\" id=\"listingtable\">";
 			$data.="<tr class=\"theader\">";
 			$n=0;
+
+			$max_fields=sizeof($this->fields);
+			$max_extra_fields=sizeof($this->extra_fields);
+			//$max=sizeof($this->fields);
+			$max=$max_fields+$max_extra_fields;
+
+			$all_fields_array=array();
+			for ($i=0;$i<$max_fields;$i++){
+				$field=array();
+				$field["mode"]="field";
+				$field["index"]=$i;
+				$tmp=sprintf("%05d",$i);
+				$all_fields_array["$tmp"."_0"]=$field;
+			};
 			
-			$max=sizeof($this->fields);
+			$i=0;
+			foreach ($this->extra_fields as $key => $ef){
+				$field=array();
+				$field["mode"]="extra";
+				$field["index"]=$key;
+				$tmp=sprintf("%05d",$ef["pos"]);
+				$all_fields_array["$tmp"."_1"."$i"]=$field;
+				$i=$i+1;
+			};	
+			ksort($all_fields_array);
+
 			//echo $max."sd";
 			//print_R($this->fields);
-			while($n < $max)
+//			$n_field=0;
+//			$n_extra_field=0;
+
+			foreach($all_fields_array as $field)
 			{
-				if(trim($this->fields[$n])=="")
-					$data.="<th nowrap>&nbsp;</th>";
-				else 
-					$data.="<th nowrap>".$this->fields[$n]."</th>";
-				$n++;
+				$n_field=$field["index"];
+				if ($field["mode"]=="field"){
+					if(trim($this->fields[$n_field])=="")
+						$data.="<th nowrap>&nbsp;</th>";
+					else
+						$data.="<th nowrap>".$this->fields[$n_field]."</th>";
+				};
+
+				if ($field["mode"]=="extra"){
+					$data.="<th nowrap>".$this->extra_fields[$n_field]["title"]."</th>";
+				}
 			}
 			//+2
-		    
-			$data.="<th nowrap>&nbsp;</th>";
-			$data.="<th nowrap>&nbsp;</th>";
-			
+
+
+			if ($this->enableMod == 0)
+			{
+				$data.="<th nowrap>&nbsp;</th>";
+			}
+			if ($this->enableDel == 0)
+			{
+				$data.="<th nowrap>&nbsp;</th>";
+			}
+
 			$data.="</tr>";
 			$col=0;
 			if (mysql_num_rows($this->rs)==0) return "<br />".LANG_ADMIN_NODATA."<br />";
-			
-			
+
+
+
 			while($rows=mysql_fetch_array($this->rs,MYSQL_NUM))
-			{	
+			{
+				$normal_fields=array();
 				if($col%2)
 					$data.="<tr class=\"darkcolor\">";
-				else 
+				else
 					$data.="<tr class=\"lightcolor\">";
-				$i=0;	
-               
+				$i=0;
+
 				$max=($this->activatelisting==1)?sizeof($rows)-1:sizeof($rows);
-				
+
 				while($i<$max)
-				{	
-					$str = $stringutil->clean_value($rows[$i]);
+				{
+					$field_name=mysql_field_name($this->rs,$i);
+					$str = $stringutil->clean_value($this->getRealValue($rows[$i],$field_name));
 					if(strlen($str) > $this->limit_display)
 					{
 						$str=substr($str,0,$this->limit_display)." ...";
-					}
+					};
 
-					if($str != "")
-						$data.="<td class=\"bodytext\">".$stringutil->cleanDescription2($str)."</td>";
-					else
-						$data.="<td class=\"bodytext\">"."&nbsp;"."</td>";
-					
+					$normal_fields[$i]=$str;
+
 					$i++;
 				}
-				
 				// TODO: activ
 				if ($this->getActivateListing() == 1) {
 					if ( $rows[$max] == 1) { //if is active, green
@@ -157,12 +228,51 @@
 					$data.="<td class=\"bodytext\" width=\"24\"><a class=\"activate\"  href=\"".$_SERVER['PHP_SELF']."?do=activate&".$this->getFirstID()."=".$rows[0]."\"><img src=\"".CONF_INDEX_URL."images/admin/activate0.ico\" title=\"".LANG_ADMIN_ACTIVATE."\"></a></td>";
 					}
 				}
-				
-				$data.="<td class=\"bodytext\" width=\"24\"><a class=\"mod\"  href=\"".$_SERVER['PHP_SELF']."?do=mod&".$this->getFirstID()."=".$rows[0]."\"><img src=\"".CONF_INDEX_URL."images/admin/mod.ico\" title=\"".LANG_ADMIN_MODIFY."\"></a></td>";
-				
 
-				
-				$data.="<td class=\"bodytext\" width=\"24\"><a class=\"del\"  href=\"".$_SERVER['PHP_SELF']."?do=del&".$this->getFirstID()."=".$rows[0]."\" onclick=\"return confirm('".LANG_ADMIN_CONFIRMDELETE."')\"><img src=\"".CONF_INDEX_URL."images/admin/del.ico\" title=\"".LANG_ADMIN_DELETE."\" ></a></td>";
+			foreach($all_fields_array as $field)
+			{
+				$str="";
+				$n_field=$field["index"];
+				if ($field["mode"]=="field"){
+					$str=$normal_fields[$n_field];
+				};
+				if ($field["mode"]=="extra"){
+					$function=$this->extra_fields[$n_field]["function"];
+					$function_class_or_object=$function["class_or_object"];
+					$function_name=$function["function"];
+					$parameter=$rows[0];
+					$function_call="";
+					if (empty($function_class_or_object)){
+						$function_call=$function_name;
+					}else{
+						$function_call=array($function_class_or_object,$function_name);
+					};
+
+
+					if (is_callable($function_call)){
+						$str=call_user_func_array($function_call,array($parameter));
+					}else{
+						$str="";//the function could not be called
+					};
+				};
+				if($str != "")
+					$data.="<td class=\"bodytext\">".$stringutil->cleanDescription2($str)."</td>";
+				else
+					$data.="<td class=\"bodytext\">"."&nbsp;"."</td>";
+			};
+
+
+				if ($this->enableMod == 0)
+				{
+					$data.="<td class=\"bodytext\" width=\"24\"><a class=\"mod\"  href=\"".$_SERVER['PHP_SELF']."?do=mod&".$this->getFirstID()."=".$rows[0]."\"><img src=\"".CONF_INDEX_URL."images/admin/mod.ico\" title=\"".LANG_ADMIN_MODIFY."\"></a></td>";
+				}
+
+
+				if ($this->enableDel == 0)
+				{
+					$data.="<td class=\"bodytext\" width=\"24\"><a class=\"del\"  href=\"".$_SERVER['PHP_SELF']."?do=del&".$this->getFirstID()."=".$rows[0]."\" onclick=\"return confirm('".LANG_ADMIN_CONFIRMDELETE."')\"><img src=\"".CONF_INDEX_URL."images/admin/del.ico\" title=\"".LANG_ADMIN_DELETE."\" ></a></td>";
+				}
+
 				$data.="</tr>\n";
 				$col++;
 			}
@@ -206,5 +316,53 @@
 		public function getParameter() {
 			return $this->parameter;
 		}
+		protected function getRealValue($id_data,$field_name){
+			if (isset($this->replace_table[$field_name])){
+				if (($this->replace_table[$field_name]["mode"]=="database")){
+					$id_column=$this->replace_table[$field_name]["id_column"];
+					$data_column=$this->replace_table[$field_name]["data_column"];
+					$table=$this->replace_table[$field_name]["table"];
+					$SQL_RV = "SELECT `$data_column` FROM `".DB_PREFIX."$table` WHERE `$id_column`='$id_data'";
+					$retidrv = mysql_query($SQL_RV);
+					if (!$retidrv) { echo( mysql_error()); }
+
+					if($rows=mysql_fetch_array($retidrv,MYSQL_NUM)){
+						return $rows[0];
+					}else{
+						return $id_data;
+					};
+				};
+				if (($this->replace_table[$field_name]["mode"]=="array")){
+					return $this->replace_table[$field_name]["array"][$id_data];
+				};
+			};
+			return $id_data;
+		}
+		public function setReplaceColumnIdFromDatabase($field,$table,$id_column,$data_column){
+			$rpl=array();
+			$rpl["mode"]="database";
+			$rpl["table"]=$table;
+			$rpl["id_column"]=$id_column;
+			$rpl["data_column"]=$data_column;
+
+			$this->replace_table[$field]=$rpl;
+		}
+		public function setReplaceColumnIdFromArray($field,$array){
+			$rpl=array();
+			$rpl["mode"]="array";
+			$rpl["array"]=$array;
+			$this->replace_table[$field]=$rpl;
+		}
+		
+		public function addNewFunctionColumn($position,$title,$function_or_method,$class_or_object){
+			$new_index=0;
+			if (!empty($this->extra_fields)) $new_index=sizeof($this->extra_fields);
+			$ef["pos"]=$position;
+			$ef["title"]=$title;
+			$ef["function"]=array("class_or_object" => $class_or_object,
+								  "function" => $function_or_method);
+			$this->extra_fields[$new_index]=$ef;
+		}
+
 	}
 ?>
