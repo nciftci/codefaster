df8e9c787cf890c7d10be62d7b6cbbc16f0ea749
lvalics <lvalics@5a6d9080-2914-11de-b161-0f7b0ef88f65>
2009-05-31 18:56:03 +0000

    function check_authentification() moved into this file

--- trunk/public_html/include/cls_authenticate.php
+++ trunk/public_html/include/cls_authenticate.php
@@ -1,4 +1,37 @@
 <?php
+class Authenticate {
+	public function check_authentification(){
+		// begin authenticate part
+		if(AUTH_TYPE == 1)
+		{
+			if (!$AUTHENTICATE)
+			{
+				header( "WWW-Authenticate: Basic realm=\"ADMIN ".CONF_SITE_NAME."\"");
+				header( "HTTP/1.0 401 Unauthorized");
+				$ft = new FastTemplate(ADMIN_TEMPLATE_CONTENT_PATH);
+				$ft->define(array("main"=>"template_firstpage.html", "content"=>"authentication_failed.html"));
+				$ft->multiple_assign_define("LANG_");
+				$ft->multiple_assign_define("CONF_");
+				$ft->parse("BODY", array("content","main"));
+				$ft->showDebugInfo(ERROR_DEBUG);
+				$ft->FastPrint();
+				exit;
+			}
+		}
+		else if(AUTH_TYPE == 2)
+		{
+			include_once(INCLUDE_PATH.'cls_session.php');
+			$sess = new MYSession();
+			if (!$sess->get(SESSION_ID))
+			{
+				$sess->set('session_url_before',$_SERVER['REQUEST_URI']);
+				header("Location: login.php");
+				exit;
+			}
+		}// end authenticate part
+
+	}
+}	
             if (empty($PHP_AUTH_USER))
             {
                     if (!empty($_SERVER) && isset($_SERVER['PHP_AUTH_USER'])) {
