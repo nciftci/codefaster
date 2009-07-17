ebfcea96b13fa8ad09e7140cf916f4aa4ffd075f
paulgfx <paulgfx@5a6d9080-2914-11de-b161-0f7b0ef88f65>
2009-07-17 14:07:17 +0000

    started to add xml load support

--- trunk/index.php
+++ trunk/index.php
@@ -4,6 +4,7 @@ include_once("./config.inc.php");
 include_once(INCLUDE_PATH."cls_fast_template.php");
 include_once(INCLUDE_PATH."cls_string.php");
 
+
 $ft = new FastTemplate(TEMPLATE_PATH);
 
 $stringutil = new String();
@@ -26,6 +27,31 @@ if($all_url_vars["page"]=="first") {
 			"content" => "sql.html"
 		)
 	);
+} elseif ($all_url_vars["page"]=="xml") {
+	$ft->define(array(
+			"main"    => "template_body.html",
+			"content" => "xml.html"
+		)
+	);
+
+
+        $dirname=INDEX_PATH."/schema";
+        $dirh = opendir($dirname);
+        $select_data="<select id='xmlfile' name='xmlfile'>";
+        if ($dirh) {
+
+            while ($filename = readdir($dirh)) {
+                $ext=strtoupper(end(explode(".", $filename)));
+
+                if ($ext!="XML") continue;
+                $select_data.=" <option value='$filename'>$filename</option>";
+            }
+            unset($filename);
+            closedir($dirh);
+        }
+        $select_data.="</select>";
+        $ft->assign("SELECT_XML_FILES",$select_data);
+
 } else {
 	$ft->define(array(
 			"main"    => "template_body.html", 
